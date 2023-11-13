93e5488ae260bd412e7ec1d515b1251f93c4e5c9
Stephen Mata <stevet.mata@gmail.com>
2023-11-12 19:00:48 -0800

    added signal results vc

---
+++ AutoPilot_Admin/Signal Results/SignalResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignalResultsViewController.swift
+//  AutoPilot_Admin
+//
+//  Created by Stephen Mata on 11/7/23.
+//
+
+import UIKit
+
+class SignalResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+}
